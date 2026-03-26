77f43baf51aaa32fa1e3800561a0c43ad689711e
Niharika <nd03366@srmist.edu.in>
2026-03-26 15:24:12 +0530

    UC6: Static functions for O, P, S

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,13 +1,22 @@
 /**
  * OOPS Banner Application
+ * UC2: Print OOPS in large ASCII banner using string concatenation
  *
  * @author Niharika
- * @version 1.0
+ * @version 2.0
  */
 public class OOPSBannerApp {
 
     public static void main(String[] args) {
-        System.out.println("OOPS");
+
+        System.out.println("  ****    ****    ****   **** ");
+        System.out.println(" *    *  *    *  *    * *    *");
+        System.out.println("*      **      **      *      *");
+        System.out.println("*      **      **      *      *");
+        System.out.println("*      **      **      *      *");
+        System.out.println(" *    *  *    *  *    * *    *");
+        System.out.println("  ****    ****    ****   **** ");
     }
 
 }
+
---
+++ OOPSBannerAppUC6.java
@@ -0,0 +1,55 @@
+public class OOPSBannerAppUC6 {
+
+    public static String[] getO() {
+        return new String[]{
+            " *** ",
+            "*   *",
+            "*   *",
+            "*   *",
+            "*   *",
+            "*   *",
+            " *** "
+        };
+    }
+
+    public static String[] getP() {
+        return new String[]{
+            "**** ",
+            "*   *",
+            "*   *",
+            "**** ",
+            "*    ",
+            "*    ",
+            "*    "
+        };
+    }
+
+    public static String[] getS() {
+        return new String[]{
+            " ****",
+            "*    ",
+            "*    ",
+            " *** ",
+            "    *",
+            "    *",
+            "**** "
+        };
+    }
+
+    public static void main(String[] args) {
+
+        String[] o = getO();
+        String[] p = getP();
+        String[] s = getS();
+
+        String[] banner = new String[7];
+
+        for (int i = 0; i < 7; i++) {
+            banner[i] = o[i] + "  " + o[i] + "  " + p[i] + "  " + s[i];
+        }
+
+        for (String line : banner) {
+            System.out.println(line);
+        }
+    }
+}
